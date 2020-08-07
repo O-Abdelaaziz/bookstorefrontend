b85c3c8ce65168ba8c93965a9f24af15fc9195cb
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2020-08-07 17:35:54 +0200

    add font to the style file

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -2,17 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book';
+import{map}from 'rxjs/operators';
+
+
+interface GetEmbeddedBooks {
+  _embedded: {
+    books: Book[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  baseUrl:string='http://localhost:8080/api/books';
+  baseUrl: string = 'http://localhost:8080/api/books';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAllBooks():Observable<Book[]>{
-    return this.http.get<Book[]>(this.baseUrl);
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<GetEmbeddedBooks>(this.baseUrl).pipe(
+      map(response=>response._embedded.books)
+    );
   }
 }
+
